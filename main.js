0bb14c0ae90e8eacfeb08fa23d8fbd2f79450e23
cganterh <cganter@futaleufu.labs.inf.utfsm.cl>
2014-05-27 16:45:51 -0400

    se agregan un par de imagenes.

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ var ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,100);
+ctx.drawImage(img,10,10); //http://www.techrepublic.com/article/preloading-and-the-javascript-image-object/
