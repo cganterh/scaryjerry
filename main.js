d8f79e7ddfe6452b140b81742a7e61cb0fbcd9d5
cristobal <cganterh@gmail.com>
2014-06-12 16:31:41 -0400

    Se agregan capas.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
+var layer1 = document.getElementById("layer1");
+var l1ctx = layer1.getContext('2d');
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,100);
-ctx.drawImage(img,10,10); //http://www.techrepublic.com/article/preloading-and-the-javascript-image-object/
+var layer2 = document.getElementById("layer2");
+var l2ctx = layer2.getContext('2d');
+
+var layer3 = document.getElementById("layer3");
+var l3ctx = layer3.getContext('2d');
+
+l1ctx.fillStyle = "#FF0000";
+l1ctx.fillRect(0,0,150,150);
+
+l2ctx.fillStyle = "#00FF00";
+l2ctx.fillRect(100,100,260,110);
+
+l3ctx.fillStyle = "#0000FF";
+l3ctx.fillRect(200,200,370,120);
